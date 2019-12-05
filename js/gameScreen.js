39de0256972cda46da867e65998be73469e8e8f3
Elshafeay <mohamedelshafeay@gmail.com>
2019-12-05 16:06:48 +0200

    Enhancing the code && adding some more assets && adding the characters and its movements, the fireBall and its bouncing

--- js/gameScreen.js
+++ js/gameScreen.js
@@ -1,14 +1,23 @@
-// Test pushing file to testing branch
-function component(width, height, color, x, y) {
+var container = document.getElementById("container")
+var Xincreament = 20
+var Yincreament = 10
+
+function component(imgSrc, x, y, Ybound, height, width) {
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
+    this.imgSrc=imgSrc
     this.update = function(){
-        ctx = myGameArea.context;   
-        ctx.fillStyle = color;
-
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        var image = document.createElement("img")
+        ctx = gameArea.context;   
+        image.src=this.imgSrc
+        if(this.y<0){this.y=0}
+        if(this.y>Ybound){this.y=Ybound}
+        image.addEventListener('load', e => {
+            ctx.drawImage(image, this.x, this.y);
+            return true
+        });
     }
     this.crashWith = function(otherobj) {
         var myleft = this.x;
@@ -20,76 +29,94 @@ function component(width, height, color, x, y) {
         var othertop = otherobj.y;
         var otherbottom = otherobj.y + (otherobj.height);
         var crash = true;
-        if ((mybottom < othertop) ||
-        (mytop > otherbottom)) {
+        if ((mytop < othertop) ||
+        (mybottom > otherbottom) ||
+        (myright < otherleft) ||
+        (myleft > otherright)) {
           crash = false;
         }
         return crash;
     }
 }
 
-var myGameArea = {
-    canvas : document.getElementById("myCanvas"),
+var gameArea = {
+    canvas : document.createElement("canvas"),
     start : function() {
+        this.canvas.width = 1530;
+        this.canvas.height = 560;
         this.context = this.canvas.getContext("2d");
-        document.body.html = this.canvas;
-        this.interval = setInterval(updateGameArea, 20);
+        container.insertBefore(this.canvas, container.childNodes[2]);
+        this.interval = setInterval(updateGameArea, 40);
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
     }
 }
 
 function startGame() {
-    myGameArea.start();
-    myGamePiece = new component(15, 80, "red", 10, 120);
-    myGamePiece2 = new component(15, 80, "red" , 1300 , 120);
-    border1 = new component(1364 , 5 , "brown" , 0 , 0);
-    border2 = new component(1364 , 5 , "brown" , 0 , 445)
+    gameArea.start();
+    gameCharLeft = new component("images/goku_blue_left.png", 40, 150, 300, 250, 150)
+    gameCharRight = new component("images/goku_yellow_right.png", 1350, 150, 300, 250, 150)
+    fireBall = new component("images/fire_ball.png", 700, 150, 450, 110, 115)
 }
 
+//we put it into setInterval to be repeated the whole time
+//this function would clear the screen and set the new coordinates
+//of each one of the characters and draw it 
+//over and over again to be like a punch of frames
 function updateGameArea() {
-    myGameArea.clear();
-    myGamePiece.update();
-    myGamePiece2.update();
-    border1.update();
-    border2.update();
+    gameArea.clear()
+    if(fireBall.crashWith(gameCharLeft)||fireBall.crashWith(gameCharRight)){
+        Xincreament *= -1
+    }
+    if(fireBall.y <= 0){
+        Yincreament *= -1
+        fireBall.y = 0
+    }
+    else if(fireBall.y >= 450){
+        Yincreament *= -1
+        fireBall.y = 450
+    }
+    fireBall.x += Xincreament
+    fireBall.y += Yincreament
+    fireBall.update()
+    gameCharLeft.update()
+    gameCharRight.update()
 }
 
-function moveUp()
-{
-    if (!myGamePiece.crashWith(border1)) {
-        myGamePiece.y -= 20;
-    }
-   
+function leftMoveUp() {
+    gameCharLeft.y -= 20;
 }
-
-function moveDown()
-{
-    if (!myGamePiece.crashWith(border2)) {
-        myGamePiece.y += 20;
-    }
+  
+function leftMoveDown() {
+    gameCharLeft.y += 20;
 }
-
-function moveUp2()
-{
-    if (!myGamePiece2.crashWith(border1)) {
-        myGamePiece2.y -= 20;
-    }
+function rightMoveUp() {
+    gameCharRight.y -= 20;
 }
-
-function moveDown2()
-{
-    if (!myGamePiece2.crashWith(border2)) {
-        myGamePiece2.y += 20;
-    }
+  
+function rightMoveDown() {
+    gameCharRight.y += 20;
 }
 
 startGame()
-document.addEventListener("keydown", (event)=>{
-    var keyName = event.key
-    if(keyName=="w"){moveUp()}
-    else if(keyName=="s"){moveDown()}
-    if(keyName=="ArrowUp"){moveUp2()}
-    else if(keyName=="ArrowDown"){moveDown2()}}
-)
+var canvas = document.getElementsByTagName('canvas')[0];
+
+document.addEventListener( 'keyup' , (event) => {
+    const keyName = event.key;
+    if (keyName == 'w') {
+        leftMoveUp()
+    }
+    else if (keyName == 's') {
+        leftMoveDown()
+    }
+    if (keyName == 'ArrowUp') {
+        rightMoveUp()
+    }
+    else if (keyName == 'ArrowDown') {
+        rightMoveDown()
+    }
+})
