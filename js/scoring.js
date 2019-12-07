998bd96b3b3e31ecb973f4c45fa52ef91550f8ab
Elshafeay <mohamedelshafeay@gmail.com>
2019-12-07 17:22:20 +0200

    making scoring live

--- js/scoring.js
+++ js/scoring.js
@@ -1,23 +1,35 @@
 var rPlayerScore = 0
 var lPlayerScore = 0
+var Rscore = document.querySelector("#Rscore")
+var Lscore = document.querySelector("#Lscore")
+Lscore.textContent=lPlayerScore
+Rscore.textContent=rPlayerScore
+
 function checkScoring(){
     if(Rgoal){
         rPlayerScore+=1;
+        Rscore.textContent=rPlayerScore
         Rgoal=0;
         loadGame()
     }
     if(Lgoal){
         lPlayerScore+=1;
+        Lscore.textContent=lPlayerScore
         Lgoal=0;
         loadGame()
     }
-
     if(rPlayerScore==3){
-        alert("We have a WINNER!, congrats player 2")
+        Rscore.textContent=rPlayerScore
+        ("We have a WINNER!, congrats player 2")
         rPlayerScore=lPlayerScore=0
+        Rscore.textContent=rPlayerScore
+        Lscore.textContent=lPlayerScore
     }
     if(lPlayerScore==3){
+        Lscore.textContent=lPlayerScore
         alert("We have a WINNER!, congrats player 1")
         rPlayerScore=lPlayerScore=0
+        Rscore.textContent=rPlayerScore
+        Lscore.textContent=lPlayerScore
     }
 }
