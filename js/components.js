3b7602bad3aaa9d6f31b3a52788d6f7c6f22ccb2
unknown <omarknighto@gmail.com>
2019-12-07 13:09:50 +0200

    adding game loading

--- js/components.js
+++ js/components.js
@@ -5,12 +5,19 @@ function component(imgSrc, x, y, Ybound, height, width) {
     this.y = y;
     this.image=new Image(this.width , this.height)
     this.image.src=imgSrc
+    ctx = gameArea.context
+    this.draw = function(){  
+        this.image.addEventListener('load', e => 
+        {
+            ctx.drawImage(this.image, this.x, this.y , this.width , this.height);
+            return true
+        })                                   
+    },
     this.update = function(){
-        ctx = gameArea.context;   
         if(this.y<0){this.y=0}
         if(this.y>Ybound){this.y=Ybound}
         ctx.drawImage(this.image, this.x, this.y , this.width , this.height);
-    }
+    },
     this.crashWith = function(otherobj) {
         var myleft = this.x;
         var myright = this.x + (this.width);
--- js/gameScreen.js
+++ js/gameScreen.js
@@ -1,10 +1,18 @@
 var gameArea = {
     canvas : document.createElement("canvas"),
-    start : function() {
+    load : function() {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight * 0.75 ;
         this.context = this.canvas.getContext("2d");
         container.insertBefore(this.canvas, container.childNodes[2]);
+        gameCharLeft = new component("images/goku_blue_left.png", 40, 150, 300, 250, 150)
+        gameCharRight = new component("images/goku_yellow_right.png", window.innerWidth - (window.innerWidth/7), 150, 300, 250, 150)
+        fireBall = new component("images/fire_ball.png", 200, 150, 450, 110, 115)
+        fireBall.draw()
+        gameCharLeft.draw()
+        gameCharRight.draw()
+    },   
+    start : function() {
         this.interval = setInterval(updateGameArea, 20);
     },
     clear : function() {
@@ -17,9 +25,6 @@ var gameArea = {
 
 function startGame() {
     gameArea.start();
-    gameCharLeft = new component("images/goku_blue_left.png", 40, 150, 300, 250, 150)
-    gameCharRight = new component("images/goku_yellow_right.png", window.innerWidth - (window.innerWidth/7), 150, 300, 250, 150)
-    fireBall = new component("images/fire_ball.png", 200, 150, 450, 110, 115)
 }
 
 function updateGameArea() {
@@ -35,8 +40,9 @@ var Xincreament = 15
 var Yincreament = 10
 var Rgoal=0
 var Lgoal=0
-startGame()
+
 var canvas = document.getElementsByTagName('canvas')[0];
+loadGame();
 
 var map = {38: false, 40: false, 119: false, 115: false, 87: false, 83: false};
 document.addEventListener( 'keydown' , (e) => {
