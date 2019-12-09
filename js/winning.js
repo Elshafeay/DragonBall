e4e259e06eeb43e4be8238fca20b1688257248d1
Elshafeay <mohamedelshafeay@gmail.com>
2019-12-09 10:53:03 +0200

    Fixing the previous commit bugs

--- js/winning.js
+++ js/winning.js
@@ -13,21 +13,6 @@ function playerTwoWinning(){
     winningTimeOut = setTimeout(endGame, 2000);
 }
 
-function endGamevar winningTimeOut
-function playerOneWinning(){
- // gameArea.drawFirstWinner()
-    loader.style.display = "block";
-    loader.textContent= "First player Wins";
-    winningTimeOut = setTimeout(endGame, 2000);
-}
-
-function playerTwoWinning(){
-//  gameArea.drawSecondWinner()
-    loader.style.display = "block";
-    loader.textContent= "second player Wins";
-    winningTimeOut = setTimeout(endGame, 2000);
-}
-
 function endGame()
 {
     resetScoreBoard();
@@ -35,20 +20,6 @@ function endGame()
     gameArea.clear();
     winningPopup();
  //   loadGame();
-}
-
-function resetScoreBoard(){
-    rPlayerScore=lPlayerScore=0
-    Lgoal=Rgoal=0
-    Rscore.textContent=rPlayerScore
-    Lscore.textContent=lPlayerScore
-}
-()
-{
-    resetScoreBoard();
-    loader.style.display = "none";
-    gameArea.clear();
-    loadGame();
 }
 
 function resetScoreBoard(){
